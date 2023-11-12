0af6e29d52007cd7f063540e73ea0c173d33ae93
David van den Bor <david@davidvandenbor.nl>
2023-11-12 14:14:30 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,14 +5,14 @@
 // =======================
 // FOR_OF LOOP!
 //========================
-// Gebruik de for-of loop om REEDS BESTAANDE HTML objecten te selecteren, zodat je door hen heen kan "loopen" om ze allemaal te wijzigen! Hier bijvoorbeeld "loopen" we langs ALLE H2 elementen om de tekstjes ervan te veranderen. Eerst maken we een variabele waarin we alle H2 elementen van je pagina of website mee selecteren:
-var subkopjes = document.querySelectorAll("h2");
+// Gebruik de for-of loop om REEDS BESTAANDE HTML objecten te selecteren, zodat je door hen heen kan "loopen" om ze te wijzigen! Hier bijvoorbeeld "loopen" we langs ALLE H2 elementen om de tekstjes ervan te veranderen. Eerst maken we een variabele waarin we alle H2 elementen van je pagina of website mee selecteren:
+var subkopjes = document.querySelectorAll("h3");
 
 // En dit is dan de For_of loop om "langs" ALLE H2 elementen te gaan, en in 1 klap de tekstinhoud te wijzigen!
 for (items of subkopjes) {
-	items.innerHTML = "Jongens";
+	items.innerHTML = "Jongen";
 }
-// je kunt ooks langs item loopen en ALLEEN SPECIFIEKE items wijzigen als je dat wilt:
+// je kunt ooks door items heen "loopen" en ALLEEN SPECIFIEKE items wijzigen als je dat wilt:
 for (items of subkopjes) {
 	subkopjes[3].innerHTML = "Dit is een Meisje";
 }
